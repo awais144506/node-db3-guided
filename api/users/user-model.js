75ad4d718d0b71604e98ecfc20e2df8863433c0c
awais144506 <awaisrasheed68@gmail.com>
2024-11-26 11:39:45 +0500

    Done

--- api/users/user-model.js
+++ api/users/user-model.js
@@ -9,7 +9,18 @@ module.exports = {
 }
 
 function findPosts(user_id) {
+  const result = db('posts as p')
+    .select('p.id as post_id', 'username', 'contents')
+    .join('users as u', 'p.user_id', 'u.id')
+    .where('u.id', user_id)
+  return result
   /*
+  select 
+p.id as post_id,u.username,contents
+from posts as  p
+join users as u
+on p.user_id = u.id
+where u.id = 3
     Implement so it resolves this structure:
 
     [
@@ -24,8 +35,20 @@ function findPosts(user_id) {
 }
 
 function find() {
-  return db('users')
+  const result = db('users as u')
+    .select('u.id as user_id', 'username')
+    .count('p.id as post_count')
+    .leftJoin('posts as p', 'u.id', 'p.user_id')
+    .groupBy('u.id')
+  return result
   /*
+  select 
+u.id as user_id,username,count(p.id) as post_count
+from users as u
+left join posts as p
+on u.id = p.user_id
+group by u.id
+
     Improve so it resolves this structure:
 
     [
@@ -44,9 +67,35 @@ function find() {
   */
 }
 
-function findById(id) {
-  return db('users').where({ id }).first()
+async function findById(id) {
+  const rows =await db('users as u')
+    .select('u.id as user_id',
+      'username',
+      'contents',
+      'p.id as post_id'
+    )
+    .leftJoin('posts as p',
+      'u.id',
+      'p.user_id')
+    .where(
+      'u.id',
+      id)
+
+  let result = rows.reduce((acc, row) => {
+    if (row.contents) {
+      acc.posts.push({ contents: row.contents, post_id: row.post_id })
+    }
+    return acc
+  }, {  user_id: rows[0].user_id, username: rows[0].username,posts: [] })
+
+  return result
   /*
+  select 
+u.id as user_id,username,p.id as post_id,contents
+from users as u
+left Join posts as p
+on u.id = p.user_id
+where u.id = 2
     Improve so it resolves this structure:
 
     {
